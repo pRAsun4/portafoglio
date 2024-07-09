c8471ffe02d182bab8205132b161df4e33476da3
Prasun Mondal <prasun0403.pm@gmail.com>
2024-07-10 00:03:26 +0530

    resume section done

---
+++ src/assets/svg/ExperienceIcon.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+function ExperienceIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      fill="none"
+    >
+      <path
+        d="M10.2287 9.88068V7.56924C10.2287 6.95621 10.4722 6.36829 10.9057 5.93482C11.3391 5.50134 11.9271 5.25781 12.5401 5.25781H17.163C17.776 5.25781 18.3639 5.50134 18.7974 5.93482C19.2309 6.36829 19.4744 6.95621 19.4744 7.56924V9.88068M5.6058 16.815L9.24631 17.7245C12.9265 18.6444 16.7766 18.6444 20.4568 17.7245L24.0973 16.815"
+        stroke="url(#paint0_linear_20_207)"
+        strokeWidth="2.31143"
+        strokeLinecap="round"
+      />
+      <path
+        d="M5.6058 12.1918C5.6058 11.5788 5.84933 10.9909 6.28281 10.5574C6.71628 10.1239 7.30421 9.88037 7.91724 9.88037H21.7858C22.3989 9.88037 22.9868 10.1239 23.4203 10.5574C23.8537 10.9909 24.0973 11.5788 24.0973 12.1918V21.4375C24.0973 22.0506 23.8537 22.6385 23.4203 23.072C22.9868 23.5054 22.3989 23.749 21.7858 23.749H7.91724C7.30421 23.749 6.71628 23.5054 6.28281 23.072C5.84933 22.6385 5.6058 22.0506 5.6058 21.4375V12.1918Z"
+        stroke="url(#paint1_linear_20_207)"
+        strokeWidth="2.31143"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.849 15.6566C15.4873 15.6566 16.0048 15.1392 16.0048 14.5009C16.0048 13.8626 15.4873 13.3452 14.849 13.3452C14.2108 13.3452 13.6933 13.8626 13.6933 14.5009C13.6933 15.1392 14.2108 15.6566 14.849 15.6566Z"
+        fill="url(#paint2_linear_20_207)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_20_207"
+          x1="14.8515"
+          y1="5.25781"
+          x2="14.8515"
+          y2="18.4144"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF9C1A" />
+          <stop offset="1" stopColor="#E80505" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_20_207"
+          x1="14.8515"
+          y1="9.88037"
+          x2="14.8515"
+          y2="23.749"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF9C1A" />
+          <stop offset="1" stopColor="#E80505" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_20_207"
+          x1="14.849"
+          y1="13.3452"
+          x2="14.849"
+          y2="15.6566"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF9C1A" />
+          <stop offset="1" stopColor="#E80505" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default ExperienceIcon;
--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -1,39 +1,134 @@
-import React from 'react'
-import EducationIcon from '../assets/svg/EducationIcon';
+import React from "react";
+import EducationIcon from "../assets/svg/EducationIcon";
+import { useSelector } from "react-redux";
+import ExperienceIcon from "../assets/svg/ExperienceIcon";
 
 function ResumeSection({ mood }) {
+  const activeSection = useSelector((state) => state.section.activeSection);
+  console.log(activeSection, "resumeSection");
+
   return (
     <div
-      className={`resume-sections w-full h-auto flex flex-col justify-start 
-        ${mood ? "bg-[#2e2e2e]" : "bg-white"
-        } py-[2.25rem] lg:px-[3rem] sm:px-[2rem] px-5 rounded-[1.25rem] gap-y-[1rem]`}
+      className={`resume-sections ${
+        activeSection ? "sec-anime" : "no-anime"
+      } w-full h-auto flex flex-col justify-start 
+        ${mood ? "dark-bg" : "light-bg"}  rounded-[1.25rem] overflow-hidden gap-y-[1rem]`}
     >
-      <div className="header relative">
+      <div className="header relative py-[2.25rem] lg:px-[3rem] sm:px-[2rem] px-5 ">
         <h2
-          className={`before-heade ${mood ? " light-text " : " dark-text"
-            } text-left xl:text-[2.5rem] sm:text-[1.5rem] text-[1.2rem] font-medium relative flex items-center`}
+          className={`before-heade ${
+            mood ? " light-text " : " dark-text"
+          } text-left xl:text-[2.5rem] sm:text-[1.5rem] text-[1.2rem] font-medium relative flex items-center`}
         >
           Resume
           <span className="header-line lg:max-w-[15.625rem] md:max-w-[12rem] sm:max-w-[10rem] max-w-[8rem] w-full h-[3px]  lg:ml-8 sm:ml-5 ml-3"></span>
         </h2>
       </div>
 
-      <div className="box-section grid grid-cols-12 items-start xl:gap-x-[3.2rem] lg:gap-x-[2rem] md:gap-x-[1.5rem] ">
-        <div className="left-box md:col-span-6 col-span-12 flex flex-col ">
+      <div className="box-section grid grid-cols-12 items-start gap-y-6 xl:gap-x-[3.2rem] lg:gap-x-[2rem] md:gap-x-[1.5rem]   pb-[2.25rem] lg:px-[3rem] sm:px-[2rem] px-5">
+        <div className="left-box md:col-span-6 col-span-12 flex flex-col gap-y-[1.25rem]">
           <div className="header-logo flex items-center ">
             <EducationIcon />
-            <h3 className=" lg:text-[1.75rem] text-[1.25rem] font-medium ">
+            <h3
+              className={` ${
+                mood ? "light-text" : "dark-text"
+              } lg:text-[1.75rem] text-[1.25rem] font-medium  ml-2`}
+            >
               Education
             </h3>
           </div>
-        </div>
-        <div className="right-box md:col-span-6 col-span-12 flex flex-col "></div>
-        <div className="about-grid-main grid grid-cols-12 lg:gap-x-[1.25rem] lg:gap-y-[1.25rem] gap-y-5 w-full h-full ">
+          <div className="resume-box p-4 flex flex-col justify-start items-start gap-y-2 rounded-lg border ">
+            <p className={` text-[0.8125rem] text-[#A89479] font-normal `}>
+              2020-2021
+            </p>
+            <h4 className={` text-[0.875rem] text-[#000] font-semibold `}>
+              Software Development{" "}
+            </h4>
+            <p className={` text-[0.8125rem] text-[#000] font-medium `}>
+              Moringa School
+            </p>
+          </div>
 
+          <div className="resume-box p-4 flex flex-col justify-start items-start gap-y-2 rounded-lg border ">
+            <p className={` text-[0.8125rem] text-[#A89479] font-normal `}>
+              2020-2021
+            </p>
+            <h4 className={` text-[0.875rem] text-[#000] font-semibold `}>
+              Software Development{" "}
+            </h4>
+            <p className={` text-[0.8125rem] text-[#000] font-medium `}>
+              Moringa School
+            </p>
+          </div>
+        </div>
+
+        <div className="right-box md:col-span-6 col-span-12 flex flex-col gap-y-[1.25rem]">
+          <div className="header-logo flex items-center ">
+            <ExperienceIcon />
+            <h3
+              className={` ${
+                mood ? "light-text" : "dark-text"
+              } lg:text-[1.75rem] text-[1.25rem] font-medium  ml-2`}
+            >
+              Experience
+            </h3>
+          </div>
+          <div className="resume-box p-4 flex flex-col justify-start items-start gap-y-2 rounded-lg border ">
+            <p className={` text-[0.8125rem] text-[#A89479] font-normal `}>
+              2020-2021
+            </p>
+            <h4 className={` text-[0.875rem] text-[#000] font-semibold `}>
+              Software Development{" "}
+            </h4>
+            <p className={` text-[0.8125rem] text-[#000] font-medium `}>
+              Moringa School
+            </p>
+          </div>
+
+          <div className="resume-box p-4 flex flex-col justify-start items-start gap-y-2 rounded-lg border ">
+            <p className={` text-[0.8125rem] text-[#A89479] font-normal `}>
+              2020-2021
+            </p>
+            <h4 className={` text-[0.875rem] text-[#000] font-semibold `}>
+              Software Development{" "}
+            </h4>
+            <p className={` text-[0.8125rem] text-[#000] font-medium `}>
+              Moringa School
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`about-skills-grid ${
+          mood ? "dark-bg" : "bg-[#E1E8EF]"
+        } grid grid-cols-12 items-start gap-y-6 xl:gap-x-[3.2rem] lg:gap-x-[2rem] md:gap-x-[1.5rem] w-full h-full   py-[2.25rem] lg:px-[3rem] sm:px-[2rem] px-5`}
+      >
+        <div className="left-box md:col-span-6 col-span-12 flex flex-col gap-y-[1.25rem]">
+          <div className="header-logo flex items-center ">
+            <h3
+              className={` ${
+                mood ? "light-text" : "dark-text"
+              } lg:text-[1.75rem] text-[1.25rem] font-medium  ml-2`}
+            >
+              Work Skills
+            </h3>
+          </div>
+        </div>
+
+        <div className="right-box md:col-span-6 col-span-12 flex flex-col gap-y-[1.25rem]">
+          <div className="header-logo flex items-center ">
+            <h3
+              className={` ${
+                mood ? "light-text" : "dark-text"
+              } lg:text-[1.75rem] text-[1.25rem] font-medium  ml-2`}
+            >
+              Soft Skills
+            </h3>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ResumeSection;
